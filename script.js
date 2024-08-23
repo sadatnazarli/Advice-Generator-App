fc804f0040b0882443225f0b2cea04b87d87b71a
Sadat <sedat.nazarli@gmail.com>
2024-08-23 14:31:34 +0400

    final commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('get-advice').addEventListener('click', function() {
     fetch('https://api.adviceslip.com/advice')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('advice-text').innerText = data.slip.advice;
+            document.getElementById('advice-text').innerText = `"${data.slip.advice}"`;
+            document.getElementById('advice-id').innerText = data.slip.id;
         });
 });
-
